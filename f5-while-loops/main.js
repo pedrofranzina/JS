03808b338cb0449212be76768cb8eb6878c05d03
pedrofranzina <pedrofranzinaa@gmail.com>
2024-07-10 23:08:27 +0100

    js

---
+++ f5-while-loops/main.js
@@ -0,0 +1,35 @@
+let counter = 0;
+while (counter > 10) {
+    console.log(counter);
+    counter = counter + 1;
+}
+
+// usem um ciclo para calcular a tabuada...
+// um ciclo que corre 10x e calcula 7 multiplicado
+// por cada 1 dos nuimeros entre 1 e 10
+
+let number = 7;
+
+while (number < 71) {
+    console.log(number);
+    number = number + 7;
+}
+
+console.log(" ")
+
+let number1 = 8;
+while (number1 < 81) {
+    console.log(number1);
+    number1 = number1 + 8;
+}
+
+console.log(" ")
+
+let number2 = 9;
+while (number2 < 91) {
+    console.log(number2);
+    number2 = number2 + 9;
+}
+
+console.log(" ")
+console.log("Fim da tabuada")
