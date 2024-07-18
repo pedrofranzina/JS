87d97fccfd5e3ffef0f17ab00800d18f4d5c0994
pedrofranzina <pedrofranzinaa@gmail.com>
2024-07-18 19:34:38 +0100

    exercrise ascii

---
+++ exercises/exercise2/main.js
@@ -0,0 +1,83 @@
+// let readlineSync = require("readline-sync")
+
+// console.log("Car or wedding?")
+// let choice = readlineSync.question("> ");
+
+// if (choice == "car") {
+
+//     console.log("        _______");
+//     console.log("       //  ||\\ \\");
+//     console.log(" _____//___||_\\ \\___");
+//     console.log(" )  _          _    \\");
+//     console.log(" |_/ \\________/ \\___|");
+//     console.log("___\\_/________\\_/______");
+// }
+
+// else if (choice === "wedding") {
+//     console.log("   ,-==-,");
+//     console.log("   \\/\\/\\/");
+//     console.log("    \\//");
+//     console.log("   .-''-.");
+//     console.log(" .'.-\"\" -.'.---.");
+//     console.log("/ /    .'\\ \\--.\'.");
+//     console.log("| |   / /| |   \\ \\");
+//     console.log("\\ \\  | | / /    | |");
+//     console.log(" '.'-:\\ \\.'    / /");
+//     console.log("   '---; '-..-'.'");
+//     console.log(" jgs    `'----'");
+// } else {
+//     console.log("Invalid choice. call for 'car' or 'wedding'")
+// }
+
+
+let readlineSync = require("readline-sync");
+
+console.log("do you wanna marry me?");
+let answer = readlineSync.question("> ");
+
+if (answer == "no") {
+    console.log("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠖⠚⠉⠁⠀⠀⠉⠙⠒⢄⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⠀⠀⠀⠀⠀⠀⢀⠔⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢢⡀⠀⠀⠀");
+    console.log("⠀⠀⠀⠀⠀⠀⡰⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣆⠀⠀");
+    console.log("⠀⠀⠀⠀⠀⢠⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢇⠀");
+    console.log("⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄");
+    console.log("⠀⠀⠀⠀⢸⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇");
+    console.log("⠀⠀⠀⠀⠸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡘");
+    console.log("⠀⠀⠀⠀⠀⢻⠀⠀⠀⠀⠀⠀⠀⢀⣴⣶⡄⠀⠀⠀⠀⠀⢀⣶⡆⠀⢠⠇");
+    console.log("⠀⠀⠀⠀⠀⠀⣣⠀⠀⠀⠀⠀⠀⠀⠙⠛⠁⠀⠀⠀⠀⠀⠈⠛⠁⡰⠃⠀");
+    console.log("⠀⠀⠀⠀⢠⠞⠋⢳⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠜⠁⠀⠀");
+    console.log("⠀⠀⠀⣰⠋⠀⠀⠀⢷⠙⠲⢤⣀⡀⠀⠀⠀⠀⠴⠴⣆⠴⠚⠁⠀⠀⠀⠀");
+    console.log("⠀⠀⣰⠃⠀⠀⠀⠀⠘⡇⠀⣀⣀⡉⠙⠒⠒⠒⡎⠉⠀⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⢠⠃⠀⠀⢶⠀⠀⠀⢳⠋⠁⠀⠙⢳⡠⠖⠚⠑⠲⡀⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⡎⠀⠀⠀⠘⣆⠀⠀⠈⢧⣀⣠⠔⡺⣧⠀⡴⡖⠦⠟⢣⠀⠀⠀⠀⠀⠀");
+    console.log("⢸⠀⠀⠀⠀⠀⢈⡷⣄⡀⠀⠀⠀⠀⠉⢹⣾⠁⠁⠀⣠⠎⠀⠀⠀⠀⠀⠀");
+    console.log("⠈⠀⠀⠀⠀⠀⡼⠆⠀⠉⢉⡝⠓⠦⠤⢾⠈⠓⠖⠚⢹⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⢰⡀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠀⠀⠀⢸⠀⠀⠀⠀⡏⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⠳⡀⠀⠀⠀⠀⠀⠀⣀⢾⠀⠀⠀⠀⣾⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⠀⠈⠐⠢⠤⠤⠔⠚⠁⠘⣆⠀⠀⢠⠋⢧⣀⣀⡼⠀⠀⠀⠀⠀⠀⠀⠀");
+    console.log("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠈⠁⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀");
+} else if (answer == "yes") {
+
+    console.log("         /\"\\");
+    console.log("     /\"\\|\\./|/\"\\");
+    console.log("    |\\./|   |\\./|");
+    console.log("    |   |   |   |");
+    console.log("    |   |>~<|   |/\"\\");
+    console.log("    |>~<|   |>~<|\\./|");
+    console.log("    |   |   |   |   |");
+    console.log("/~T\\|   |   =[@]=   |");
+    console.log("|_/ |   |   |   |   |");
+    console.log("|   | ~   ~   ~ |   |");
+    console.log("|~< |             ~ |");
+    console.log("|   '               |");
+    console.log("\\                   |");
+    console.log(" \\                 /");
+    console.log("  \\               /");
+    console.log("   \\.            /");
+    console.log("     |          |");
+    console.log("     |          |");
+} else {
+    console.log("invalid answer... don't stress me :(")
+}
