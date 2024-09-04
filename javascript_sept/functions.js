ef844dc74e961eb9f5b1580ef10ea6ff08bc3593
pedrofranzina <pedrofranzinaa@gmail.com>
2024-09-04 23:30:44 +0100

    firstfunction

---
+++ javascript_sept/functions.js
@@ -0,0 +1,11 @@
+function myFirstFunction(i) {
+    for (let i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {
+            console.log("even");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+myFirstFunction(20);
