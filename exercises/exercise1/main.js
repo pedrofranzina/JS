bcbe265b0adc77d5557f1d717ad26c698d5488ea
pedrofranzina <pedrofranzinaa@gmail.com>
2024-09-05 16:20:21 +0100

    adjust exercise to function

--- exercises/exercise1/main.js
+++ exercises/exercise1/main.js
@@ -1,10 +1,15 @@
 let readlineSync = require("readline-sync")
 
-console.log("Please enter your first name:")
-let username = readlineSync.question("> ");
+function greeting(firstname, lastname) {
 
-console.log("Please enter your last name:")
-let lastName = readlineSync.question("> ");
+    console.log("Please enter your first name:")
+    let username = readlineSync.question("> ");
 
-console.log("Hello " + username + " " + lastName + ", welcome to the Learn to Code course.")
+    console.log("Please enter your last name:")
+    let lastName = readlineSync.question("> ");
 
+    console.log("Hello " + username + " " + lastName + ", welcome to the Learn to Code course.")
+
+}
+
+greeting()
