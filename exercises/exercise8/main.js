2fed936785e67377a26e8b6864341832862c3a11
pedrofranzina <pedrofranzinaa@gmail.com>
2024-09-09 00:02:38 +0100

    adjustments

--- exercises/exercise8/main.js
+++ exercises/exercise8/main.js
@@ -1,11 +1,11 @@
 
 let numbers = [2, 5, 10, 11, 30, 18];
-let n = 2;
-let result = "The choosen number isn't in the list"
+let n = 100;
+let result = "The chosen number isn't in the list"
 
 for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] == n) {
-        result = "The choosen number is in the list"
+        result = "The chosen number is in the list"
     }
 }
 console.log(result);
--- exercises/exercise9/main.js
+++ exercises/exercise9/main.js
@@ -1,7 +1,7 @@
 let names = ["Josie", "Catherin", "Bob", "Max", "Hannah", "Alex"];
 
 for (let i = 0; i < names.length; i++) {
-    if (i % 2 == 0) {
+    if (i % 2 == 1) {
         console.log(names[i]);
     }
 }
